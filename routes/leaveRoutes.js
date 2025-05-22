23f1325327d4519bbbf99b073b3cfecea581e457
Your Name <your.email@example.com>
2025-05-22 14:03:40 +0530

    Initial backend code commit

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -4,5 +4,15 @@ const { submitLeave } = require('../controllers/leaveController');
 const Leave = require('../models/Leave');
 
 router.post('/submit', submitLeave);
+// routes/leaveRoutes.js
+router.get('/all', async (req, res) => {
+  try {
+    const leaves = await Leave.find(); // not LeaveModel
+    res.json(leaves);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
 
 module.exports = router;
